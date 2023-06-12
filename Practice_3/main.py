23893aef74e8b50abdf753b518803169401d7e8a
Phung Van Quy <phungvanquy97@gmail.com>
2023-06-12 21:11:04 +0300

    add Practice 3

---
+++ Practice_3/main.py
@@ -0,0 +1,33 @@
+from concurrent import futures
+import grpc
+import os
+import sys
+
+
+""" Method 1 """
+import services_pb2
+# type from service 1
+print(services_pb2.SumRequest)
+
+# type from service 2
+print(services_pb2.ReadFileRequest)
+
+
+
+
+
+# """ Method 2 """
+# # Very important to change the current working directory
+# os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# protobufs_path = os.path.abspath("../protobufs")
+# sys.path.append(protobufs_path)
+
+# services_pb2, services_pb2_grpc = grpc.protos_and_services(
+#     "services.proto"
+# )
+
+# # type from service 1
+# print(services_pb2.SumRequest)
+
+# # # type from service 2
+# print(services_pb2.ReadFileRequest)
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Practice 1: ordinary example (compile gprc proto with external-tool)
 
 ## Practice 2: experimental example (compile gprc proto inside python file)
+
+## Practice 3: nested proto files (import statement)
+
+[Reference](https://protobuf.dev/programming-guides/proto3/#fieldreserved)
